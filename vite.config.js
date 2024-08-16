21db34cafe63a405bfb8203a7e16eac274ad7fd3
Lo <low3@student.douglascollege.ca>
2024-08-16 10:33:18 -0700

    Update config

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,10 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',  // Replace with your backend server's port
-        changeOrigin: true,
-      },
-    },
+  build: {
+    outDir: 'build',  // Set the output directory to "build"
   },
 });
